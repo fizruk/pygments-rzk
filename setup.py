35b0d0ab2c154ca9a369ba3ec3e77185d52ee7f3
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2023-07-11 00:54:49 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygments-rzk',
-    version='0.1.2',
+    version='0.1.3',
     description='Pygments lexer for Rzk language (of proof assistant for synthetic ∞-categories).',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
